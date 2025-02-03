ba71a3be45f03a58b1638d5417b00f91bb7d13b6
MaciejOstaszewski <xaero300@gmail.com>
2025-02-03 20:48:05 +0100

    -m

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Option 1: Quick enable for all origins
+  app.enableCors();
+  // or more restrictively
+  // app.enableCors({ origin: 'https://your-frontend-domain' });
+
+  // Start the app
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
